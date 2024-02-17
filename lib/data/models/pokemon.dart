eec8a4b2ae092a8915e0f3f5e33f7372a8a3c6f9
oodyseuss <lucifer.zamess@gmail.com>
2024-02-17 22:38:35 +0300

    created a model for pokemon

---
+++ lib/data/models/pokemon.dart
@@ -0,0 +1,15 @@
+class Pokemon {
+  final int id;
+  final String name;
+  final List<String> type;
+  final int weight;
+  final int height;
+
+  Pokemon({
+    required this.weight,
+    required this.height,
+    required this.id,
+    required this.name,
+    required this.type,
+  });
+}
