5a99dc6f6af514cbce25a6bbdbb393e338450443
oodyseuss <lucifer.zamess@gmail.com>
2024-02-21 01:47:41 +0300

    moved the text style to a sepaate file

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+TextStyle textStyleAbout = const TextStyle(
+  fontSize: 18,
+  color: Colors.black,
+  fontWeight: FontWeight.normal,
+);
+
+TextStyle textStyleAboutBold = const TextStyle(
+  fontSize: 18,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle textStyleName = const TextStyle(
+  fontSize: 20,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle textStyleHome = const TextStyle(
+  fontSize: 23,
+  color: Colors.black,
+);
