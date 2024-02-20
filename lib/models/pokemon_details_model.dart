63d53429dc6dd4cbd01b1718935ff8fc7589c6b1
oodyseuss <lucifer.zamess@gmail.com>
2024-02-21 01:44:55 +0300

    commented on the id

--- lib/models/pokemon_details_model.dart
+++ lib/models/pokemon_details_model.dart
@@ -1,5 +1,5 @@
 class PokemonDetailsModel {
-  final int id;
+//  final int id;
   final String name;
   final List<String> type;
   final int weight;
@@ -10,7 +10,7 @@ class PokemonDetailsModel {
     required this.frontImageUrl,
     required this.weight,
     required this.height,
-    required this.id,
+//    required this.id,
     required this.name,
     required this.type,
   });
